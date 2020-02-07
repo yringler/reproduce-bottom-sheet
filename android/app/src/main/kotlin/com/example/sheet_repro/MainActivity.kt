80638c44032c5061196f84012aac49d28de0f067
Yehuda Ringler <guitarguy613@gmail.com>
2020-02-06 22:00:48 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/sheet_repro/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sheet_repro
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
